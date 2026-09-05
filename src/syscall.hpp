f8ef3bbf6416f4a937b64f606eac4832e7ba3e8d
noahware <noahswtf@icloud.com>
2026-09-05 13:40:14 +0100

    Added syscall templated caller

--- src/syscall.hpp
+++ src/syscall.hpp
@@ -5,4 +5,21 @@ namespace ac
 {
     void init();
     void* stub_of(string_view_t syscall);
+
+    template <class T = void, class... Args>
+    T syscall(const string_view_t name, Args... args)
+    {
+        const void* stub = stub_of(name);
+
+        if (!stub)
+        {
+            // todo: raise exception
+            return {};
+        }
+
+        using syscall_fn = T(*)(Args...);
+        const auto fn = reinterpret_cast<syscall_fn>(stub);
+
+        return fn(args...);
+    }
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,6 @@
 #include "../src/syscall.hpp"
 #include "../src/insn.hpp"
+#include <Windows.h>
 #include <algorithm>
 #include <format>
 #include <array>
@@ -10,19 +11,10 @@ int main()
 {
     ac::init();
 
-    std::array<std::uint8_t, 4> bytes = { 0xc1, 0x00, 0x00, 0xD4 }; // little-endian
-    
-    const auto insn = ac::insn::svc::parse(bytes);
-    const auto new_insn = ac::insn::svc::encode(0x6);
+    LARGE_INTEGER time = { };
+    const auto status = ac::syscall<NTSTATUS>("ZwQuerySystemTime", &time);
 
-    const auto new_bytes = new_insn.to_bytes();
-
-    if (std::ranges::equal(bytes, new_bytes))
-    {
-        LOG("bytes are eq");
-    }
-
-	LOG("armcall 0x{:X}", insn ? insn->imm() : 0);
+	LOG("armcall status 0x{:X}, time: 0x{:X}", status, time.QuadPart);
 
 	return 0;
 }
