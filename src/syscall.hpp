7863323362747507d0989e9b8cdaea07107dbbfe
noahware <noahswtf@icloud.com>
2026-09-05 14:20:01 +0100

    Added #define helpers

--- src/syscall.hpp
+++ src/syscall.hpp
@@ -1,12 +1,24 @@
 #pragma once
 #include "deps.hpp"
+#include <Windows.h>
+
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+#define AC_SYSCALL(name, ...)        ::ac::syscall(#name, __VA_ARGS__)
+#define AC_SYSCALL_AS(T, name, ...)  ::ac::syscall<T>(#name, __VA_ARGS__)
+#elif defined(__GNUC__) || defined(__clang__)
+#define AC_SYSCALL(name, ...)        ::ac::syscall(#name, ##__VA_ARGS__)
+#define AC_SYSCALL_AS(T, name, ...)  ::ac::syscall<T>(#name, ##__VA_ARGS__)
+#else
+#define AC_SYSCALL(name, ...)        ::ac::syscall(#name __VA_OPT__(,) __VA_ARGS__)
+#define AC_SYSCALL_AS(T, name, ...)  ::ac::syscall<T>(#name __VA_OPT__(,) __VA_ARGS__)
+#endif
 
 namespace ac
 {
     void init();
     void* stub_of(string_view_t syscall);
 
-    template <class T = void, class... Args>
+    template <class T = NTSTATUS, class... Args>
     T syscall(const string_view_t name, Args... args)
     {
         const void* stub = stub_of(name);
--- tests/main.cpp
+++ tests/main.cpp
@@ -12,7 +12,7 @@ int main()
     ac::init();
 
     LARGE_INTEGER time = { };
-    const auto status = ac::syscall<NTSTATUS>("ZwQuerySystemTime", &time);
+    const auto status = AC_SYSCALL(ZwQuerySystemTime, &time);
 
 	LOG("armcall status 0x{:X}, time: 0x{:X}", status, time.QuadPart);
 
